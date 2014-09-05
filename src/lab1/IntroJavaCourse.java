617bbbe430e14d9bcb64b707f9c5cbef8b876fd7
Bill S. <wj@suetholz.net>
2014-09-04 19:17:09 -0500

    Initial Check-in with source as provided.

---
+++ src/lab1/IntroJavaCourse.java
@@ -0,0 +1,50 @@
+package lab1;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroJavaCourse {
+    String courseName;
+    private String courseNumber;
+    private double credits;
+    private String prerequisites;
+
+    public IntroJavaCourse(String courseName, String courseNumber) {
+        this.courseName = courseName;
+        this.courseNumber = courseNumber;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+        public void setCredits(double credits) {
+        if(credits < 0 || credits > 5.0) {
+            System.out.println(
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.setCredits(credits);
+    }
+
+}
---
+++ src/lab1/IntroToProgrammingCourse.java
@@ -0,0 +1,62 @@
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Describe responsibilities here. NOTE: this class has NO PREREQUISITES!
+ * Do not change this fact.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroToProgrammingCourse {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+
+    public IntroToProgrammingCourse(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseNumber = courseNumber;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public final void setCourseName(String courseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+
+    
+}
