15e39e227f1dc7614e3bca2014bfe2cf1dd0884f
Bill S. <wj@suetholz.net>
2014-09-07 21:08:31 -0500

    Update comments in main routines to handle the assignment requirements.

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,7 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * In the startup class document what you think is good or bad about this
+ * architecture from the perspective of using abstraction and inheritance.
+ *  (Remember, a startup class must have a main method)
+ * 
+ * In order to have the flexibility to override the getter and setter methods in 
+ * child classes, I basically had to define most of the methods as abstract, because I
+ * didn't want to have the properties be protected.  I wanted them to be private.
+ * With the properties private, I could not have the properties and default getters
+ * and setters defined in the abstract parent class.  I did define concrete methods
+ * for the credits property, because my reading of that property, suggested that
+ * it was defining a global rule for the restrictions on the values that could be
+ * assigned to it.  Because of that global rule, I felt it was better to have the
+ * validation defined one place, and that validation not be allowed to be overridden.
+ *
+ * 
  */
 package lab1;
 
@@ -14,6 +26,7 @@ import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
+	// The Liskov Substitution is used here to generalize the array of courses.
 	SchoolCourse[] courses = {
 	    new IntroToProgrammingCourse("Introduction To Programming", "CS-104"),
 	    new IntroJavaCourse("Introduction to the Java Programming Language", "CS-110"),
@@ -30,9 +43,7 @@ public class Main {
 	    credits = (Math.round(credits*10) / 10.0);	// round to nearest tenths place
 	    course.setCredits(credits);
 	    
-	    // Would have been nice here to be able to easily tell all the courses that 
-	    // allowed setting prerequisites, but that wouldn't have changed needing
-	    // to know which course in order to use the correct prerequisite...
+	    // Need to know which course in order to use the correct prerequisite...
 	    if (course instanceof IntroJavaCourse) {
 		IntroJavaCourse introJavaCourse = (IntroJavaCourse) course;
 		introJavaCourse.setPrerequisites("Introduction To Programming");
--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -1,7 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * In the startup class document what you think is good or bad about this
+ * architecture from the perspective of using abstraction and inheritance.
+ * (Remember, a startup class must have a main method)
+ *
+ * There is no real inheritance as when using interfaces because everything defined
+ * by the interfaces are abstract, so they have to be redefined/re-implemented for
+ * each class implementing an interface.
+ * 
+ * I think of inheritance, as the ability to reuse well tested code from the parent
+ * class with out having to always rewrite the same routines again and again.  Interfaces
+ * force you to rewrite all methods defined by the interface which is basically used
+ * as a contract defining which methods any class that implements an interface 
+ * is going to be forced to define.  The only way you would get inheritance when
+ * using interfaces, is if you defined a parent class with the appropriate concrete
+ * methods, but that leads to less flexibility down the road, because java only allows
+ * direct inheritance not side by side inheritance of classes.
+ * 
+ * At least with interfaces I can simplify the getting of course prerequisites, by 
+ * specifying and interface for the getter and setter of that property.
+ * 
+ * In order to enforce the same school wide value restrictions on the values that
+ * the credits property is allowed to be set to, I also implemented a helper static
+ * method that should be called from any implementation of the setter method for 
+ * that property.  This seems rather forced, and prone to errors down the road.  
+ * In other words, I don't really like it!  But, I want that to be a global restriction.
+ * What is the best way to accomplish that?  I also still believe that the
+ * the getter and setter methods for the credits property should be included in
+ * the SchoolCourse interface contract.  Seems to be mutually exclusive....
+ * 
  */
 package lab2;
 
@@ -14,6 +40,7 @@ import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
+	// The Liskov Substitution is used here to generalize the array of courses.
 	SchoolCourse[] courses = {
 	    new IntroToProgrammingCourse("Introduction To Programming", "CS-104"),
 	    new IntroJavaCourse("Introduction to the Java Programming Language", "CS-110"),
@@ -27,9 +54,9 @@ public class Main {
 	    if (credits < 0.5) {
 		credits = 0.5;
 	    }
-	    credits = (Math.round(credits*10) / 10.0);	// round to nearest tenths place
+	    credits = (Math.round(credits * 10) / 10.0);	// round to nearest tenths place
 	    course.setCredits(credits);
-	    
+
 	    // Need to know which course in order to use the correct prerequisite...
 	    if (course instanceof IntroJavaCourse) {
 		IntroJavaCourse introJavaCourse = (IntroJavaCourse) course;
@@ -53,7 +80,10 @@ public class Main {
 	    // Unlike with the abstract class version of this, we are able to
 	    // be able to easily tell all the courses that we have a getter 
 	    // for prerequisites.
-	    if (course instanceof CoursePrerequisites ) {
+	    //
+	    // Because of the additional interface CoursePrerequisites, I can use
+	    // the Liskov Substitution
+	    if (course instanceof CoursePrerequisites) {
 		CoursePrerequisites coursePrerequisites = (CoursePrerequisites) course;
 		sb.append("\nCourse Prerequisites: ");
 		sb.append(coursePrerequisites.getPrerequisites());
